84420414b5c1beb711375e1ac56ea4e68b832370
Sameer B <sameerflyleaf@gmail.com>
2022-10-10 19:04:01 -0700

    building routes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
-const { Post, Comment, User } = require('../models');
-const withAuth = require('../utils/auth');
+const { application } = require('express');
+const { Post, Comment, User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
+router.post('/', async (req, res) => {
+    try {
+        const newPost = await Post.create({
+            ...req.body,
+            user_id: 1
+        })
+        res.status(200).json(newPost);
+    } catch (err) {
+        res.status(500).json(err);
+      }
+});
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,5 +2,14 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({include: [User]})
+        const posts = postData.map(post => post.get({ plain: true }));
+        res.render('homepage', {posts});
+    } catch (err) {
+        res.status(500).json(err.message);
+    }
+})
 
 module.exports = router;
